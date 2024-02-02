122ef8f33b6dd9207dbd94890ae20137bcb52538
Lydia Lanier <158217265+llanier15@users.noreply.github.com>
2024-02-02 07:40:17 -0600

    Update help_button.py

--- help_button.py
+++ help_button.py
@@ -8,10 +8,10 @@ class HelpButton():
 		self.screen_rect = screen.get_rect()
 		
 		# Set the dimensions of the button.
-		self.width, self.height = 180, 56
-		self.button_color = (0, 0, 0)
+		self.width, self.height = 200, 50
+		self.button_color = (255, 0, 0)
 		self.text_color = (255, 255, 255)
-		self.font = pygame.font.SysFont('arialblack', 36)
+		self.font = pygame.font.SysFont(None, 48)
 		
 		# Buttons rect and center it.
 		self.rect = pygame.Rect(0, 0, self.width, self.height)
@@ -21,14 +21,14 @@ class HelpButton():
 		# Prepping button message.
 		self.prep_msg(msg)
 		
-		def prep_msg(self, msg):
-			"""Turn msg into a rendered img and center text."""
-			self.msg_img = self.font.render(msg, True, self.text_color,
+	def prep_msg(self, msg):
+		"""Turn msg into a rendered img and center text."""
+		self.msg_img = self.font.render(msg, True, self.text_color,
 			                               self.button_color)
-			self.msg_img_rect = self.msg_img.get_rect()
-			self.msg_img_rect.center = self.rect.center
+		self.msg_img_rect = self.msg_img.get_rect()
+		self.msg_img_rect.center = self.rect.center
 			
-		def draw_help(self):
-			"""Draw blank button and message."""
-			self.screen.fill(self.button_color, self.rect)
-			self.screen.blit(self.msg_img, self.msg_img_rect)
+	def draw_help(self):
+		"""Draw blank button and message."""
+		self.screen.fill(self.button_color, self.rect)
+		self.screen.blit(self.msg_img, self.msg_img_rect)
