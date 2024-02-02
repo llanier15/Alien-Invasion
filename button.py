217eba82b8afe06fcfb8c95b4f006a34f0bf47f3
Lydia Lanier <158217265+llanier15@users.noreply.github.com>
2024-02-02 07:38:00 -0600

    Update button.py

--- button.py
+++ button.py
@@ -15,7 +15,8 @@ class Button():
 		
 		# Build the button's rect object and center it.
 		self.rect = pygame.Rect(0, 0, self.width, self.height)
-		self.rect.center = self.screen_rect.center
+		self.rect.centerx = self.screen_rect.centerx - 150
+		self.rect.centery = self.screen_rect.centery
 		
 		# The button message needs to be prepped only once.
 		self.prep_msg(msg)
